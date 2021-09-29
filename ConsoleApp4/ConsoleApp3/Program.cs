691de742f8a80c2521ac8ce5ebe91020ec0ddd86
AntonMetodiev <toni2riemail@abv.bg>
2021-09-29 09:06:54 +0300

    nekvo suobshtenie

---
+++ ConsoleApp4/ConsoleApp3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var even = nums.Where(x => x % 2 == 0);
+            var odd = nums.Where(x => x % 2 != 0);
+            var result = even.Sum() - odd.Sum();
+            Console.WriteLine(result);
+        }
+    }
+}
